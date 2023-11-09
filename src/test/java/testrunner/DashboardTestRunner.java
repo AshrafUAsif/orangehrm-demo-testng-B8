842f4345a1ca707097cfc0963bb5a2419d25cb54
AshrafUAsif <108550504+AshrafUAsif@users.noreply.github.com>
2023-11-09 19:40:11 +0600

    create test suite and automate userlogin by creating new users

--- src/test/java/testrunner/DashboardTestRunner.java
+++ src/test/java/testrunner/DashboardTestRunner.java
@@ -1,21 +1,38 @@
 package testrunner;
 
+import com.github.javafaker.Faker;
 import config.Setup;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
+import utils.Utils;
+
+import java.io.IOException;
 
 public class DashboardTestRunner extends Setup {
     @BeforeTest
-    public void login(){
+    public void login() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.doLogin("Admin","admin123");
+        loginPage.doLogin("Admin", "admin123");
     }
 
     @Test(priority = 1)
-    public void createUser(){
+    public void createUser() throws IOException, ParseException, InterruptedException {
         DashboardPage dashboardPage = new DashboardPage(driver);
-        dashboardPage.createUser("Test","User","testuser123","Asif199069");
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String userName = faker.name().username();
+        String password = faker.internet().password();
+        dashboardPage.createUser(firstName, lastName, userName, password);
+        String textTitleExpected = driver.findElement(By.xpath("//*[contains(text(),\"Personal Details\")]")).getText();
+        Thread.sleep(5000);
+        if (textTitleExpected.contains("Personal Details")) {
+            Utils.saveEmployeeInfo(firstName, lastName, userName, password);
+        }
+        Thread.sleep(5000);
     }
 }
